f5eb2f01e5625e050139ba93e5e8a551cef580dd
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-03-04 19:45:20 +0100

    add gitignore

---
+++ src/List/ArrayList.h
@@ -0,0 +1,14 @@
+//
+// Created by prf on 04/03/19.
+//
+
+#ifndef STRUTTURE_ASD_ARRAYLIST_H
+#define STRUTTURE_ASD_ARRAYLIST_H
+
+
+class ArrayList {
+
+};
+
+
+#endif //STRUTTURE_ASD_ARRAYLIST_H
---
+++ src/List/List.h
@@ -0,0 +1,14 @@
+//
+// Created by prf on 04/03/19.
+//
+
+#ifndef STRUTTURE_ASD_LIST_H
+#define STRUTTURE_ASD_LIST_H
+
+
+class List {
+
+};
+
+
+#endif //STRUTTURE_ASD_LIST_H
