d44754fb1efb48b1100b0ab1830752f071a41033
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-03-05 10:24:00 +0100

    Add some ArrayList tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,43 @@
+
 //
 // Created by prf on 04/03/19.
 //
 
+#include <iostream>
+#include "List/ArrayList.h"
+
+
+
+int test_ArrayList(){
+    // TODO: add complete test suite
+    ArrayList<int> lst(3);
+    lst.add(1,1);
+    lst.add(2,2);
+    lst.add(3,3);
+    ArrayList<int> lst2(lst);
+    lst2.add(100, 2);
+    lst2.add(-23,3);
+    lst2.add(-1,1);
+    lst2.add(-2000,lst2.size());
+    std::cout << "list#1:" <<lst << std::endl;
+    std::cout << "list#2:" <<lst2 << std::endl;
+    lst.add(100,4);
+
+    ArrayList<int> elst;
+
+    elst.add(100,1);
+    elst.remove(1);
+    std::cout << "list#3 empty?:" << elst.is_empty() << std::endl;
+    elst.remove(1);
+
+    lst2.remove(3);
+    lst2.remove(lst2.size());
+    std::cout << "list#2:" <<lst2 << std::endl;
+
+    return 0;
+}
+int main(){
+    test_ArrayList();
+
+    return 0;
+}
