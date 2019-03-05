c0ed8538c21bc5164f4ff896383f00b25e6cb9ca
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-03-05 19:02:59 +0100

    Add LinkedList test function

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 
 #include <iostream>
 #include "List/ArrayList.h"
-
-
+#include "List/LinkedList.h"
+using std::cout;
+using std::endl;
 
 int test_ArrayList(){
     // TODO: add complete test suite
@@ -36,8 +37,79 @@ int test_ArrayList(){
 
     return 0;
 }
+
+
+int test_LinkedList(){
+
+    LinkedList<int> lst;
+    lst.pushFront(1000);
+    lst.pushFront(200);
+    lst.pushFront(80);
+    cout << "List#1: " <<lst<< endl;
+
+    LinkedList<int> lst2;
+    lst2.pushBack(1000);
+    lst2.pushBack(200);
+    lst2.pushBack(80);
+    cout << "List#2: " <<lst2 << endl;
+
+    lst2.remove(lst2.begin());
+    lst2.remove(lst2.begin());
+    lst2.remove(lst2.begin());
+    bool v = lst2.is_empty(); // -> True
+    lst2.remove(lst2.begin());
+    cout << "List#2: " << lst2 << endl;
+
+    LinkedList<int> lst3;
+    lst3.pushBack(10);
+    lst3.pushBack(100);
+    lst3.pushBack(1000);
+    cout << "List#3: " << lst3 << endl;
+    lst3.popFront();
+    lst3.popBack();
+    cout << "List#3: " << lst3 << endl; // ->[100]
+
+    LinkedList<int> lst4(lst);
+    cout << "List#4: " << lst4 << endl;
+    lst4.clear();
+    //Check if lst4 isn't a shallowed copy of lst3
+    cout << "List#4: " << lst4 << endl;
+    cout << "List#3: " << lst3 << endl;
+
+    LinkedList<std::string> lst5("Hello World", 10);
+    cout << "List#5: " << lst5 << endl;
+    LinkedList<std::string> lst6(lst5);
+    cout << "List#5: " << lst5 << endl;
+    cout << "List#6: " << lst6 << endl;
+    cout << "List#5 == List#6?: " << std::boolalpha <<(lst5 == lst6) << endl;
+
+    lst6.popBack();
+    cout << "List#5: " << lst5 << endl;
+    cout << "List#6: " << lst6 << endl;
+    cout << "List#5 == List#6?: " << std::boolalpha <<(lst5 == lst6) << endl;
+
+    lst5.popBack();
+    lst6.update("Changed Value",lst6.begin());
+    cout << "List#5: " << lst5 << endl;
+    cout << "List#6: " << lst6 << endl;
+    cout << "List#5 == List#6?: " << std::boolalpha <<(lst5 == lst6) << endl;
+
+    lst2.pushBack(444);
+    lst2.pushBack(555);
+    lst2.pushBack(666);
+    cout << "List#2: " << lst2 << endl;
+    cout << "List#3: " << lst3 << endl;
+    lst3 = lst2; // -> lst3 = [ 444,555,666]
+    lst2.pushBack(777);
+    cout << "List#2: " << lst2 << endl;
+    cout << "List#3: " << lst3 << endl;
+
+    return 0;
+}
+
 int main(){
-    test_ArrayList();
+    //test_ArrayList();
+    test_LinkedList();
 
     return 0;
 }
