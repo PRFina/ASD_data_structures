bb5d79fabba150598f5ca156efb50f454d61ac54
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-03-07 18:08:51 +0100

    Add Binary Tree implementation with array with ArrayBinTree class. Add insertion, getters and inspection operators

---
+++ src/BinTree/ArrayBinTree.h
@@ -0,0 +1,263 @@
+//
+// Created by prf on 07/03/19.
+//
+
+#ifndef STRUTTURE_ASD_ARRAYBINTREE_H
+#define STRUTTURE_ASD_ARRAYBINTREE_H
+
+#include "BinTree.h"
+
+
+
+
+template <class T>
+class ArrayBinTree: public BinTree<T, int> {
+    static const int NIL = -1;
+public:
+    typedef typename BinTree<T, int>::value_type value_type;
+    typedef typename BinTree<T, int>::Node Node;
+
+    ArrayBinTree();
+    void print();
+
+    void add_root(value_type val) override;
+    void add_left_child(Node n) override;
+    void add_left_child(value_type val, Node n);
+    void add_right_child(Node n) override;
+    void add_right_child(value_type val, Node n);
+
+    bool is_empty() override;
+    bool left_child_empty(Node n) override;
+
+    bool right_child_empty(Node n) override;
+    Node get_root() override;
+    Node get_parent(Node n) override;
+    Node get_left_child(Node n) override;
+    Node get_right_child(Node n) override;
+
+    value_type get(Node n) override;
+
+    void update(value_type val, Node n) override;
+
+private:
+    struct _Cell {
+        Node parent;
+        Node left;
+        Node right;
+        value_type value;
+    };
+
+    _Cell* _data;
+    size_t _size;
+    size_t _MAX_SIZE;
+    Node _root;
+    Node _free_pos;
+
+    bool isFull();
+
+
+
+};
+
+template<class T>
+ArrayBinTree<T>::ArrayBinTree() {
+    _MAX_SIZE = 100;
+    _data = new _Cell[_MAX_SIZE];
+    _size = 0;
+    _root = NIL;
+    _free_pos = 0;
+
+    for (int i = 0; i < _MAX_SIZE ; i++) {
+        _data[i].left = (i+1) % _MAX_SIZE;
+    }
+
+}
+
+template<class T>
+typename ArrayBinTree<T>::value_type ArrayBinTree<T>::get(Node n) {
+    if (n != NIL)
+        return _data[n].value;
+}
+
+template<class T>
+void ArrayBinTree<T>::add_left_child(Node n) {
+
+    if(is_empty())
+        throw std::domain_error("Empty Tree");
+    if (isFull())
+        throw std::domain_error("Tree is full");
+    if (n == NIL)
+        throw std::domain_error("Null Node");
+    if (_data[n].left != NIL)
+        throw std::domain_error("Node already exists");
+    else{
+        Node new_node = _free_pos;
+        _free_pos = _data[_free_pos].left;
+
+        _data[n].left = new_node;
+        _data[new_node].parent = n;
+
+        _data[new_node].left = NIL;
+        _data[new_node].right = NIL;
+
+        _size++;
+
+    }
+}
+
+template<class T>
+void ArrayBinTree<T>::add_left_child(value_type val, Node n) {
+    if(is_empty())
+        throw std::domain_error("Empty Tree");
+    if (isFull())
+        throw std::domain_error("Tree is full");
+    if (n == NIL)
+        throw std::domain_error("Null Node");
+    if (_data[n].left != NIL)
+        throw std::domain_error("Node already exists");
+    else{
+        Node new_node = _free_pos;
+        _free_pos = _data[_free_pos].left;
+
+        _data[n].left = new_node;
+        _data[new_node].parent = n;
+        _data[new_node].value = val;
+        _data[new_node].left = NIL;
+        _data[new_node].right = NIL;
+
+        _size++;
+
+    }
+}
+
+template<class T>
+void ArrayBinTree<T>::add_right_child(Node n) {
+    if(is_empty())
+        throw std::domain_error("Empty Tree");
+    if (isFull())
+        throw std::domain_error("Tree is full");
+    if (n == NIL)
+        throw std::domain_error("Null Node");
+    if (_data[n].right != NIL)
+        throw std::domain_error("Node already exists");
+    else{
+        Node new_node = _free_pos;
+        _free_pos = _data[_free_pos].left;
+
+        _data[n].right = new_node;
+        _data[new_node].parent = n;
+
+        _data[new_node].left = NIL;
+        _data[new_node].right = NIL;
+
+        _size++;
+    }
+
+}
+
+template<class T>
+void ArrayBinTree<T>::add_right_child(value_type val, ArrayBinTree::Node n) {
+    if(is_empty())
+        throw std::domain_error("Empty Tree");
+    if (isFull())
+        throw std::domain_error("Tree is full");
+    if (n == NIL)
+        throw std::domain_error("Null Node");
+    if (_data[n].right != NIL)
+        throw std::domain_error("Node already exists");
+    else{
+        Node new_node = _free_pos;
+        _free_pos = _data[_free_pos].left;
+
+        _data[n].right = new_node;
+        _data[new_node].parent = n;
+        _data[new_node].value = val;
+        _data[new_node].left = NIL;
+        _data[new_node].right = NIL;
+
+        _size++;
+    }
+}
+
+template<class T>
+void ArrayBinTree<T>::update(value_type val, Node n) {
+    if (n != NIL)
+        _data[n].value = val;
+}
+
+template<class T>
+bool ArrayBinTree<T>::is_empty() {
+    return (_size == 0);
+}
+
+template<class T>
+bool ArrayBinTree<T>::left_child_empty(Node n) {
+    return (_data[n].left == NIL);
+}
+
+template<class T>
+typename ArrayBinTree<T>::Node ArrayBinTree<T>::get_root() {
+    return _root;
+}
+
+template<class T>
+typename ArrayBinTree<T>::Node ArrayBinTree<T>::get_parent(Node n) {
+    if ( n != _root)
+        return _data[n].parent;
+    else
+        return n;
+}
+
+template<class T>
+typename ArrayBinTree<T>::Node ArrayBinTree<T>::get_left_child(Node n) {
+    if ( !left_child_empty(n))
+        return _data[n].left;
+    else
+        return n;
+}
+
+template<class T>
+typename ArrayBinTree<T>::Node ArrayBinTree<T>::get_right_child(Node n) {
+    if ( !right_child_empty(n))
+        return _data[n].right;
+    else
+        return n;
+}
+
+template<class T>
+bool ArrayBinTree<T>::right_child_empty(ArrayBinTree<T>::Node n) {
+    return (_data[n].left == NIL);
+}
+
+template<class T>
+void ArrayBinTree<T>::add_root(value_type val) {
+    if (is_empty() && _root == NIL){
+        _root = _free_pos;
+        _free_pos = _data[_free_pos].left;
+
+        _data[_root].value = val;
+        _data[_root].left = NIL;
+        _data[_root].right = NIL;
+
+        _size++;
+    }
+
+}
+
+
+
+#endif //STRUTTURE_ASD_ARRAYBINTREE_H
+
+template<class T>
+void ArrayBinTree<T>::print() {
+    for (int i = 0; i < _MAX_SIZE ; ++i) {
+        std::cout << "array pos: " << i << "   (" << _data[i].value << ", " << _data[i].parent << ", "
+        << _data[i].left << ", " << _data[i].right << ")" << std::endl;
+    }
+
+}
+
+template<class T>
+bool ArrayBinTree<T>::isFull() {
+    return(_size == _MAX_SIZE);
+}
